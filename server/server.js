766f72b5f4288f4cfaa342e04d2b09d8ab2b287b
Hardattsinh Zala <zalahardatt@gmail.com>
2025-09-30 01:27:06 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const errorFunction = require("./middlewares/error-func");
 const cors = require("cors");
 
 const PORT = process.env.PORT || 4000;
-const CLIENT_URL = process.env.CLIENT_URL || 'https://agrocare-ai-server.onrender.com';
+const CLIENT_URL = process.env.CLIENT_URL || 'https://agro-care-ai.vercel.app';
 
 const corsOption = {
   origin: [CLIENT_URL, 'http://localhost:5173', 'http://127.0.0.1:3000'],
